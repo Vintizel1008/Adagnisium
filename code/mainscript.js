70ce247de1f412cd29a3df5431e9d2e24d99a2a8
Vintizel1008 <147973018+Vintizel1008@users.noreply.github.com>
2023-10-15 15:30:40 +0700

    Add files via upload

---
+++ code/mainscript.js
@@ -0,0 +1,20 @@
+
+// Sticky NavBar
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+// Login button
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
